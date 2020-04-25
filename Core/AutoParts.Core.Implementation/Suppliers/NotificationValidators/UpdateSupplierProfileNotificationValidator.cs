c03995a19d15a8cd13362405d70dc02dd063a2ba
Anton Bubel <a.bubel@itransition.com>
2020-04-25 18:03:46 +0300

    Add validator for the update supplier profile notification

---
+++ Core/AutoParts.Core.Implementation/Suppliers/NotificationValidators/UpdateSupplierProfileNotificationValidator.cs
@@ -0,0 +1,38 @@
+﻿namespace AutoParts.Core.Implementation.Suppliers.NotificationValidators
+{
+    using FluentValidation;
+
+    using Contracts.Suppliers.Notifications;
+
+    using Constants.ValidationConstants;
+
+    public class UpdateSupplierProfileNotificationValidator : AbstractValidator<UpdateSupplierProfileNotification>
+    {
+        public UpdateSupplierProfileNotificationValidator()
+        {
+            RuleFor(notification => notification.SalesEmail)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(notification => notification.SalesPhoneNumber)
+                .MinimumLength(UserValidationConstants.PhoneNumberMinLength)
+                .MaximumLength(UserValidationConstants.PhoneNumberMaxLength)
+                .Matches(UserValidationConstants.PhoneNumberFormat)
+                .NotEmpty();
+
+            RuleFor(notification => notification.OrganizationName)
+                .NotEmpty()
+                .MaximumLength(ValidationConstants.DefaultMaxLength);
+
+            RuleFor(notification => notification.OrganizationAddress)
+                .NotEmpty()
+                .MaximumLength(ValidationConstants.SupplierOrganizationAddressMaxLength);
+
+            RuleFor(notification => notification.OrganizationDescription)
+                .MaximumLength(ValidationConstants.SupplierOrganizationDescriptionMaxLength);
+
+            RuleFor(notification => notification.Website)
+                .MaximumLength(UserValidationConstants.DefaultMaxLength);
+        }
+    }
+}

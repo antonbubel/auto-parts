ce74fc2c793f2246e1d4f19ac7894243ec9b2ccb
Anton Bubel <a.bubel@itransition.com>
2020-04-25 13:56:58 +0300

    Add validator for the send email notification

---
+++ Core/AutoParts.Core.Implementation/Emails/NotificationValidators/SendEmailNotificationValidator.cs
@@ -0,0 +1,20 @@
+﻿namespace AutoParts.Core.Implementation.Emails.NotificationValidators
+{
+    using FluentValidation;
+
+    using Contracts.Emails.Notifications;
+
+    public class SendEmailNotificationValidator : AbstractValidator<SendEmailNotification>
+    {
+        public SendEmailNotificationValidator()
+        {
+            RuleFor(notification => notification.ToEmail)
+                .NotEmpty()
+                .EmailAddress();
+
+            RuleFor(notification => notification.TemplateId)
+                .NotNull()
+                .NotEmpty();
+        }
+    }
+}

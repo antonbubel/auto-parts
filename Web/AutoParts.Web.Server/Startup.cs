e53a05aa6cd0f976b37be7d5749c0fabcdd5f1c4
Anton Bubel <a.bubel@itransition.com>
2020-05-12 00:40:12 +0300

    Map CatalogService in the startup.cs

--- Web/AutoParts.Web.Server/Startup.cs
+++ Web/AutoParts.Web.Server/Startup.cs
@@ -162,6 +162,8 @@
 
                 endpoints.MapGrpcService<SupplierService>().EnableGrpcWeb();
 
+                endpoints.MapGrpcService<CatalogService>().EnableGrpcWeb();
+
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");

d5bf0fe9e5cb13e42672660d7fd636c28291c195
Anton Bubel <a.bubel@itransition.com>
2020-05-12 00:08:20 +0300

    Implement back-end for the auto parts catalogs

---
+++ Web/AutoParts.Web.Server/MappingProfiles/CatalogMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿namespace AutoParts.Web.Server.MappingProfiles
+{
+    using AutoMapper;
+
+    using Protos;
+
+    using Core.Contracts.Catalogs.Models;
+
+    public class CatalogMappingProfile : Profile
+    {
+        public CatalogMappingProfile()
+        {
+            CreateMap<CatalogModel, Catalog>()
+                .ForMember(catalog => catalog.Id, conf => conf.MapFrom(catalogModel => catalogModel.Id))
+                .ForMember(catalog => catalog.Name, conf => conf.MapFrom(catalogModel => catalogModel.Name));
+
+            CreateMap<SubCatalog, SubCatalog>()
+                .ForMember(catalog => catalog.Id, conf => conf.MapFrom(catalogModel => catalogModel.Id))
+                .ForMember(catalog => catalog.Name, conf => conf.MapFrom(catalogModel => catalogModel.Name))
+                .ForMember(catalog => catalog.BaseCatalog, conf => conf.MapFrom(catalogModel => catalogModel.BaseCatalog));
+        }
+    }
+}
---
+++ Web/AutoParts.Web.Server/Services/CatalogService.cs
@@ -0,0 +1,77 @@
+﻿namespace AutoParts.Web.Server.Services
+{
+    using AutoMapper;
+
+    using Grpc.Core;
+
+    using MediatR;
+
+    using Microsoft.AspNetCore.Authorization;
+
+    using System.Threading.Tasks;
+
+    using Protos;
+
+    using Core.Contracts.Catalogs.Models;
+    using Core.Contracts.Catalogs.Requests;
+
+    using Infrastructure.Exceptions;
+
+    public class CatalogService : GrpcCatalogService.GrpcCatalogServiceBase
+    {
+        private readonly IMapper mapper;
+        private readonly IMediator mediator;
+
+        public CatalogService(IMapper mapper, IMediator mediator)
+        {
+            this.mapper = mapper;
+            this.mediator = mediator;
+        }
+
+        [AllowAnonymous]
+        public override async Task<GetAutoPartsCatalogsResponse> GetCatalogs(GetAutoPartsCatalogsRequest request, ServerCallContext context)
+        {
+            var catalogs = await mediator.Send(new GetCatalogsRequest());
+            var response = new GetAutoPartsCatalogsResponse();
+
+            mapper.Map(catalogs, response.Catalogs);
+
+            return response;
+        }
+
+        [AllowAnonymous]
+        public override async Task<GetAutoPartsSubCatalogsResponse> GetSubCatalogs(GetAutoPartsSubCatalogsRequest request, ServerCallContext context)
+        {
+            var subCatalogs = await mediator.Send(new GetSubCatalogsRequest { CatalogId = request.CatalogId });
+            var response = new GetAutoPartsSubCatalogsResponse();
+
+            mapper.Map(subCatalogs, response.Catalogs);
+
+            return response;
+        }
+
+        [AllowAnonymous]
+        public override async Task<GetAutoPartsSubCatalogResponse> GetSubCatalog(GetAutoPartsSubCatalogRequest request, ServerCallContext context)
+        {
+            SubCatalogModel subCatalog;
+
+            try
+            {
+                subCatalog = await mediator.Send(new GetSubCatalogByIdRequest { SubCatalogId = request.SubCatalogId });
+            }
+            catch (NotFoundException)
+            {
+                return new GetAutoPartsSubCatalogResponse
+                {
+                    Status = ResponseStatus.NotFound
+                };
+            }
+
+            return new GetAutoPartsSubCatalogResponse
+            {
+                Status = ResponseStatus.Ok,
+                Model = mapper.Map<SubCatalog>(subCatalog)
+            };
+        }
+    }
+}
